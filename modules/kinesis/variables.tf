0f7df7d7d3e77a06ed873e521eb54d5073046c31
Miguel Ramirez Ramirez Cortes <miguel.ricardo@Miguels-MacBook-Pro.local>
2023-07-17 12:25:02 -0500

    changing variable name to default

--- modules/kinesis/variables.tf
+++ modules/kinesis/variables.tf
@@ -1,18 +1,17 @@
 variable "environment" {
-  description = "Development Environment"
-  
-  type = string
-  name = "dev"
+  description = "Development Environment" 
+  type    = string
+  default = "qa"
 }
 
 variable "s3_bucket_landing_name" {
   description = "Kinesis Firehose will send data to this bucket"
   type        = string
-  name        = "dataeng-landing-zone-us-east-2-kinesis-test"
+  default        = "dataeng-landing-zone-us-east-2-kinesis-test"
 }
 
 variable "s3_bucket_output_name" {
   description = "Glue will store crawler data here"
   type        = string
-  name        = "dataeng-output-zone-us-east-2-kinesis-test"
+  default        = "dataeng-output-zone-us-east-2-kinesis-test"
 }
