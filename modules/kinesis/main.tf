6b7e4074ef5f0ce41add253529531c6d46c37af1
Miguel Ramirez Ramirez Cortes <miguel.ricardo@Miguels-MacBook-Pro.local>
2023-07-17 12:20:57 -0500

    Fixing s3 bucket name

--- modules/kinesis/main.tf
+++ modules/kinesis/main.tf
@@ -1,13 +1,13 @@
 module "s3_bucket_output" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = var.s3_bucket_name
+  bucket = var.s3_bucket_output_name
   acl    = "private"
 }
 
 module "s3_bucket_landing" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = var.s3_bucket_name
+  bucket = var.s3_bucket_landing_name
   acl    = "private"
 }
